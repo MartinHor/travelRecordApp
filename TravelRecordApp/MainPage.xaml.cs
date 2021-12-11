3479233930bb42b4b347abc1a73f74cb3e74a670
MartinUcsi <martinhor5578@gmail.com>
2021-12-12 01:33:50 +0800

    Add login page and navigate to homepage

--- TravelRecordApp/MainPage.xaml.cs
+++ TravelRecordApp/MainPage.xaml.cs
@@ -14,5 +14,22 @@ namespace TravelRecordApp
         {
             InitializeComponent();
         }
+
+        void loginButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+
+            if (isEmailEmpty || isPasswordEmpty)
+            {
+                // do not navigate
+            }
+            else
+            {
+                // navigate
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+
     }
 }
